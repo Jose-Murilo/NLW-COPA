52c9ad1bfc386c29427e3bd0c50d63bd1e209e67
Jose Murilo <murilojucas360@hotmail.com>
2022-11-03 13:39:10 -0300

    add auteration main.js

--- main.js
+++ main.js
@@ -21,27 +21,23 @@ function createCard(date, day, games) {
 }
 
 
-document.querySelector('#app').innerHTML = `
-        <header>
-            <img src="./assets/logo.svg" alt="Logo da NLW">
-        </header>
+document.querySelector('#cards').innerHTML = 
+    `
 
-        <main id="cards">
-            ${createCard("24/11", "Terça", 
-            createGame("switzerland", "7:00", "cameroon") +
-            createGame("portugal", "13:00", "ghana") +
-            createGame("brazil", "16:00", "serbia")
-            )}
+        ${createCard("24/11", "Terça", 
+        createGame("switzerland", "7:00", "cameroon") +
+        createGame("portugal", "13:00", "ghana") +
+        createGame("brazil", "16:00", "serbia")
+        )}
 
-            ${createCard("28/11", "Quinta", createGame("cameroon", "7:00", "serbia") +
-            createGame("brazil", "13:00", "switzerland") +
-            createGame("portugal", "16:00", "uruguay")
-            )}
+        ${createCard("28/11", "Quinta", createGame("cameroon", "7:00", "serbia") +
+        createGame("brazil", "13:00", "switzerland") +
+        createGame("portugal", "16:00", "uruguay")
+        )}
 
-            ${createCard("02/12", "Terça", 
-            createGame("south-korea", "12:00", "portugal") + 
-            createGame("serbia", "13:00", "switzerland") + 
-            createGame("cameroon", "16:00", "brazil")
-            )}
-        </main>
-`
+        ${createCard("02/12", "Terça", 
+        createGame("south-korea", "12:00", "portugal") + 
+        createGame("serbia", "13:00", "switzerland") + 
+        createGame("cameroon", "16:00", "brazil")
+        )}
+    `
